0b29069a3baa92a33944dee64a85be75582e0e29
Pollock Nag <75268620+Pollock-Nag@users.noreply.github.com>
2023-05-02 15:36:45 +0600

    added login flow truthy and falsy E2E test

--- client/cypress/e2e/spec.cy.js
+++ client/cypress/e2e/spec.cy.js
@@ -1,9 +1,32 @@
-const delay = 500;
+const delay = 1000;
 
 describe('template spec', () => {
-  it('passes', () => {
+  it('should create an user and login to main page to and check whether main page contains ParkLink', () => {
     cy.visit('/');
 
+    // Truthy test
+    cy.wait(delay);
+    cy.get('button').click();
+    cy.wait(delay);
+    cy.get('button').click();
+    cy.wait(delay);
+    cy.get('button').click();
+    cy.wait(delay);
+    cy.get('input').type('12345678910');
+    cy.wait(delay);
+    cy.get('button').click();
+    cy.wait(delay);
+    cy.get('#password').type('123456');
+    cy.wait(delay);
+    cy.get('button').click();
+    cy.contains('ParkLink');
+
+    // logged in to the main page
+  });
+
+  it('should show password does not match if login credentials are wrong', () => {
+    //   // falsy test
+    cy.visit('/');
     cy.wait(delay);
     cy.get('button').click();
     cy.wait(delay);
@@ -11,7 +34,9 @@ describe('template spec', () => {
     cy.wait(delay);
     cy.get('button').click();
     cy.get('input').type('12345678910');
-    cy.get('button').contains('Next').click();
-    // cy.get('button').contains('Create Account').click();
+    cy.get('button').click();
+    cy.get('#password').type('1234567');
+    cy.get('button').click();
+    cy.contains("Password doesn't match");
   });
 });
