00eae57846b6c5f6ba3205703a93f4590ff1f62e
Mahamud <mahamud@gamil.com>
2023-05-02 10:08:21 +0600

    Modified the server to write a server starting function  and start the server

--- server/server.js
+++ server/server.js
@@ -1,47 +1,13 @@
-const express = require('express');
-const cors = require('cors');
-const morgan = require('morgan');
-const app = express();
-const mongoose = require('./database/connection');
-const authRouter = require('./router/authRoute');
-const appRouter = require('./router/appRoute');
-const cloudinary = require('cloudinary').v2;
+const app = require('./app');
+const { PORT } = require('./config');
+const { connectToDatabase } = require('./database/connection');
 
-cloudinary.config({
-  cloud_name: 'di31yslny',
-  api_key: '353563475778261',
-  api_secret: 'DZzF-pPefnAXmT8QSIbkgyxy16o',
-});
+async function startServer() {
+  await connectToDatabase();
 
-//***middleware */
-app.use(express.json());
-app.use(cors());
-app.use(morgan('tiny')); // tiny log of my requests
-app.disable('x-powered-by'); // less hackers know about our stack
+  app.listen(PORT, () => {
+    console.log(`🚀  http://localhost:${PORT}`);
+  });
+}
 
-const port = 8080;
-app.post('/upload', async (req, res) => {
-  // console.log(req);
-  try {
-    const result = await cloudinary.uploader.upload(
-      req.files.file.tempFilePath
-    );
-    res.json(result.secure_url);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ err: err });
-  }
-});
-
-app.get('/', (req, res) => {
-  res.status(201).json('Home get request');
-});
-// api routers
-app.use('/auth', authRouter);
-app.use('/app', appRouter);
-
-app.listen(port, () => {
-  console.log(`🚀  http://localhost:${port}`);
-});
-
-module.exports = app;
+startServer();
