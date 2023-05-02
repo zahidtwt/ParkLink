078dbbbf2c37e610050f9e390f0d3bd935d2aa8b
Mahamud <mahamud@gamil.com>
2023-05-02 10:07:34 +0600

    Replaced bcrypt with bcryptjs

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,5 @@
 const UserModel = require('../model/User.model');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const ENV = require('../config');
 module.exports = authController = {
